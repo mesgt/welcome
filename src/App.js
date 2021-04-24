ac9935b70f2488a4bb828bf0ae73479501e6eb57
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-04-24 13:27:26 -0500

    Added rest API ChatEngine credentials.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
-import { CHatEngine } from 'react-chat-engine';
+import { ChatEngine } from 'react-chat-engine';
 
 import './App.css';
 
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="2ba2bfa5-265b-4d82-bca2-185c615e9830"
+            userName="AllPowerful"
+            userSecret="password"
+        />
+    );
+}
+
+export default App;
