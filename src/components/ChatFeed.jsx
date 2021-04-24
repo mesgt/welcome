c9244c61e524502a061a548a037f2485fd063de1
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-04-24 14:17:59 -0500

    Added chatfeed structure.

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,9 +1,46 @@
+import MessageForm from './MessageForm';
+import MyMessage from './MyMessage';
+import TheirMessage from './TheirMessage';
+
+
 const ChatFeed = (props) => {
-    console.log(props);
+    // console.log(props);
+    const { chats, activeChat, userName, messages } = props;
+
+    const chat = chats && chats[activeChat];
+
+    const renderMessages = () => {
+        const keys = Object.keys(messages);
+        // console.log(keys)
+        return keys.map((key, index) => {
+            const message = messages[key];
+            const lastMessageKey = index === 0 ? null : keys[index-1];
+            const isMyMessage = userName === message.sender.userName;
+
+            return (
+                <div key={`msg_${index}`} style={{width: '100%'}}>
+                    <div className="message-block">
+                        {
+                            isMyMessage
+                            ? <MyMessage />
+                            : <TheirMessage />
+                        }
+                    </div>
+                    <div className="read-receipts" style={{marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px'}}>
+                        read-receipts
+                    </div>
+                </div>
+            )
+        })
+    }
+
+    renderMessages();
 
     return (
-        <div> 
-            ChatFeed
+        <div className="chat-feed"> 
+            <div className="chat-title-container">
+                <div className="chat-title">{chat?.title}</div>
+            </div>
         </div>
     )
 }
