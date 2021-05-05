6f4a78f0a5e4666119fc1fc474cae4c13005677b
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-05-04 20:38:23 -0500

    Added login jsx page.

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ export function App() {
 	return (
 		<ChatEngine
 			height='100vh'
-			userName='AllPowerful'
-			userSecret='password'
+			userName='Rob_Smith'
+			userSecret='zxcvbnm'
 			projectID='2ba2bfa5-265b-4d82-bca2-185c615e9830'
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
 		/>
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import axios from 'axios';
+
+const LoginForm = () => {
+    const [ username, setUsername ] = useState('');
+    const [ password, setPassword ] = useState('');
+
+    const handleSubmit = () => {}
+
+    return (
+        <div className="wrapper">
+            <div className="form">
+                <h1 className="title">Chat Application</h1>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="User Name" required />
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
+                    <div align="center">
+                        <button type="submit" className="button">
+                            <span>Start Chatting!</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
